23f3a36f28c595cb7bdd22a98f2950b6b98299d2
seobeomseok <sbslc2000@gmail.com>
2024-03-05 14:33:53 +0900

    refactor: templateMethod 패턴 적용

--- src/xunit/TestCaseTest.java
+++ src/xunit/TestCaseTest.java
@@ -10,28 +10,14 @@ public class TestCaseTest extends TestCase {
     public TestCaseTest(String name) {
         super(name);
     }
-
-    WasRun wasRun;
-
-    /**
-     * TestCaseTest 역시 TestCase를 상속받은 것이므로
-     * setUp을 통해 초기 로직을 수행 가능
-     */
-    @Override
-    public void setUp() {
-        wasRun = new WasRun("testMethod");
-    }
-
+    
     /**
      * 테스트케이스를 상속한 클래스의 run이 정상적으로 수행되어야 한다.
      */
-    public void testRunning() {
+    public void testTemplateMethod() {
+        WasRun wasRun = new WasRun("testMethod");
         wasRun.run();
         Assert.assertEquals("setUp testMethod", wasRun.log);
     }
 
-    public void testSetUp() {
-        wasRun.run();
-        Assert.assertEquals("setUp testMethod", wasRun.log);
-    }
 }
