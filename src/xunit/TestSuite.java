eecb8f64ec3b860c7841cbedbd4f69f75d53d4e7
seobeomseok <sbslc2000@gmail.com>
2024-03-05 15:30:59 +0900

    refactor: 리플렉션을 통한 TestCase 자동 생성

--- src/xunit/TestSuite.java
+++ src/xunit/TestSuite.java
@@ -1,11 +1,28 @@
 package src.xunit;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestSuite implements Test {
 
     List<Test> tests = new ArrayList<>();
+
+    public TestSuite(Class<? extends Test> testClass) {
+        Arrays.stream(testClass.getDeclaredMethods())
+                .filter(m -> m.getName().startsWith("test"))
+                .forEach(m -> {
+                            try {
+                                add(testClass.getConstructor(String.class).newInstance(m.getName()));
+                            } catch (Exception e) {
+                                throw new RuntimeException();
+                            }
+                        }
+                );
+    }
+
+    public TestSuite() {
+    }
 
     public void add(Test test) {
         tests.add(test);
