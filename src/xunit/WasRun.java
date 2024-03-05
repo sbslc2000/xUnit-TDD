c74bcb9c44334a16a502766c82d766bc7cca885f
seobeomseok <sbslc2000@gmail.com>
2024-03-05 11:58:30 +0900

    refactor, 테스트할 메서드 이름을 받아 수행시키는 run 메서드 구현

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -1,10 +1,14 @@
 package src.xunit;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class WasRun {
+    private final String name;
     public boolean wasRun;
 
     public WasRun(String name) {
-
+        this.name = name;
     }
 
     public void testMethod() {
@@ -12,6 +16,11 @@ public class WasRun {
     }
 
     public void run() {
-
+        try {
+            Method method = getClass().getMethod(name);
+            method.invoke(this);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
